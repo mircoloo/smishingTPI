c8ca678bed89692956d8026ddd98f49937b6619b
Mirco <bismirco@gmail.com>
2022-07-06 12:23:10 +0200

    added Links

--- client/src/pages/Statistics.js
+++ client/src/pages/Statistics.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Links from '../components/Links'
+
 
 const Statistics = () => {
   return (
     <>
     <h1 className='text-center'>Statistics</h1>
     <p>Work in progress...</p>
+    <Links/>
     </>
    
   )
